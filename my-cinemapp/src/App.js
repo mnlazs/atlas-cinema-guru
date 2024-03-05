3fc4cc4414079273f61b122f1e77a79b170874a2
mnlazs <mnl.azs@gmail.com>
2024-03-05 13:11:43 -0600

    updated

--- my-cinemapp/src/App.js
+++ my-cinemapp/src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Dashboard from './components/Dashboard';
+import Authentication from './components/Authentication';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      axios.post('/api/auth/', {}, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      }).then(response => {
+        setIsLoggedIn(true);
+        setUserUsername(response.data.username);
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  }, []);
+
   return (
-    <div className="App">
-    </div>
+    isLoggedIn ? <Dashboard /> : <Authentication />
   );
 }
 
